51b852a67f97f8a57f5b41ba5e4d1776162ef483
Doeke Norg <doekenorg@gmail.com>
2024-08-15 09:31:12 +0200

    Add markup options to TextField

--- src/Field/TextField.php
+++ src/Field/TextField.php
@@ -5,8 +5,6 @@ namespace DataKit\DataViews\Field;
 /**
  * Represents a field that is rendered as normal text.
  *
- * @todo  Add size and weight
- *
  * @since $ver$
  */
 final class TextField extends Field {
@@ -18,4 +16,74 @@ final class TextField extends Field {
 	 * @var string
 	 */
 	protected string $render = 'datakit_fields.text';
+
+	/**
+	 * Returns an instance that adds break lines on the text.
+	 *
+	 * @since $ver$
+	 *
+	 * @return self The field.
+	 */
+	public function break(): self {
+		$clone                   = clone $this;
+		$clone->context['nl2br'] = true;
+
+		return $clone;
+	}
+
+	/**
+	 * Returns an instance that adds break lines on the text.
+	 *
+	 * @since $ver$
+	 *
+	 * @return self The field.
+	 */
+	public function inline(): self {
+		$clone                   = clone $this;
+		$clone->context['nl2br'] = false;
+
+		return $clone;
+	}
+
+	/**
+	 * Returns an instance with a specific font weight.
+	 *
+	 * @since $ver$
+	 *
+	 * @return self The field.
+	 */
+	public function weight( string $weight = '' ): self {
+		$clone                    = clone $this;
+		$clone->context['weight'] = $weight;
+
+		return $clone;
+	}
+
+	/**
+	 * Returns an instance which is italic or roman.
+	 *
+	 * @since $ver$
+	 *
+	 * @param bool $is_italic Whether the text should be italic.
+	 *
+	 * @return self The field.
+	 */
+	public function italic( bool $is_italic = true ): self {
+		$clone                    = clone $this;
+		$clone->context['italic'] = $is_italic;
+
+		return $clone;
+	}
+
+	/**
+	 * @inheritDoc
+	 * @since $ver$
+	 */
+	protected function default_context(): array {
+		return [
+			'nl2br'  => false,
+			'weight' => '',
+			'italic' => false,
+		];
+	}
 }
